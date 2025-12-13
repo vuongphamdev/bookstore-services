fc75c5c9e3baeb6d74cb0541a930395962f68123
Vuong Pham <phamnhuvuong96@gmail.com>
2025-12-13 07:38:18 +0700

    fix: Correctly default `top` query parameter to 10 when undefined or non-numeric.

--- src/controllers/report.controller.ts
+++ src/controllers/report.controller.ts
@@ -6,7 +6,7 @@ export const getTopSellingProducts = async (
   req: Request<any, any, any, TGetTopSellingProductsRequestQueryParams>,
   res: Response
 ): Promise<Response> => {
-  const top = Number(req.query.top) ?? 10;
+  const top = Number(req.query.top ?? 10);
   const bestSellingProducts = await reportService.getBestSellingProducts(top);
   return Responses.success(res, `Top ${top} best-selling products retrieved successfully`, bestSellingProducts);
 };
