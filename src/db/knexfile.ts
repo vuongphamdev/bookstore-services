e1c0c4179291e13e2e15693b061f6cb49eff466c
Vuong Pham <phamnhuvuong96@gmail.com>
2025-12-29 12:36:51 +0700

    feat: Add staging and production environments to knexfile and consolidate database configuration.

--- src/db/knexfile.ts
+++ src/db/knexfile.ts
@@ -2,23 +2,28 @@ import type { Knex } from 'knex';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-const config: { [key: string]: Knex.Config } = {
-  development: {
-    client: 'mysql2',
-    connection: {
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT),
-      database: process.env.DB_NAME,
-      user: process.env.DB_MIGRATION_USER,
-      password: process.env.DB_MIGRATION_PASSWORD,
-    },
-    migrations: {
-      directory: './migrations',
-    },
-    seeds: {
-      directory: './seeds',
-    },
+const dbConfig: Knex.Config = {
+  client: 'mysql2',
+  connection: {
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    database: process.env.DB_NAME,
+    user: process.env.DB_MIGRATION_USER || process.env.DB_USER,
+    password: process.env.DB_MIGRATION_PASSWORD || process.env.DB_PASSWORD,
+  },
+  migrations: {
+    directory: './migrations',
+  },
+  seeds: {
+    directory: './seeds',
   },
 };
 
+const config: { [key: string]: Knex.Config } = {
+  development: dbConfig,
+  staging: dbConfig,
+  production: dbConfig,
+};
+
+module.exports = config;
 export default config;
